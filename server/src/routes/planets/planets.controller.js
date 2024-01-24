e9ab057cf7cc6895fe8a3de3da96c09352369cca
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-01-24 21:20:42 +0500

    ENDING

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -3,7 +3,7 @@
 const planets = require("../../models/planets.model");
 
 function getAllPlanets(req, res) {
-  return res.status(200).json(planets); // Return the 'planets' array
+  return res.status(200).json(planets); // Return the 'planets' arr
 }
 
 module.exports = {
