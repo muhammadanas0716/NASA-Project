133b07a85ed7cd4173e043155e3c26ebc6c49797
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-01-24 14:41:49 +0500

    Add cors package and enable CORS in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 // Main app file to store all the routes and middleware (express app)
 
 const express = require("express");
+const cors = require("cors");
 
 // In-project IMPORTS
 const planetRouter = require("./routes/planets/planets.router");
@@ -9,6 +10,7 @@ const planetRouter = require("./routes/planets/planets.router");
 const app = express();
 
 // Parse any incoming JSON from incomming requests
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(planetRouter);
 
